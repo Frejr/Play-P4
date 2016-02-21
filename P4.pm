2e837c343b46de38553c4c5179bae6bf6ad36d28
Rafal Matczak <rav@faun.buka.one.pl>
2016-02-22 00:22:06 +0100

    Send SMS

--- P4.pm
+++ P4.pm
@@ -42,7 +42,6 @@ if ( -r "$ENV{HOME}/.play" ) { $auth = do "$ENV{HOME}/.play"; print "\nOK\n" };
 $form{'IDToken1'} = $auth->{'login'} ? $auth->{'login'} : ""; # nr telefonu
 $form{'IDToken2'} = $auth->{'password'} ? $auth->{'password'} : ""; # hasło
 
-#print $form -> {'action'}, "\n";
 $res = $ua -> post( "https://logowanie.play.pl/".$form -> {'action'}, \%form );
 
 my $tree = HTML::TreeBuilder->new_from_content( $res -> content );
@@ -66,6 +65,26 @@ foreach my $input ( @inputs ) {
 }
 
 $res = $ua -> post( 'https://bramka.play.pl/composer/'.$form -> {'action'}, \%form );
-print $res -> content;
-#print Dumper $res;
+# SMS compose
+my $tree = HTML::TreeBuilder->new_from_content( $res -> content );
+my $form = $tree -> look_down( _tag => "form" );
+my @inputs = $form -> look_down( _tag => "input" );
+my $textarea = $form -> look_down( _tag => 'textarea' );
+my %form;
+foreach my $input ( @inputs ) {
+  my $name = defined $input->{name} ? $input->{"name"} : '';
+  print "$name = \"$input->{'value'}\"\n";
+  $form{$name}=$input->{'value'};
+}
+$form{recipients} = $ARGV[0];
+$form{sendform} = 'on';
+$form{$textarea->{name}}=<STDIN>;
+$form{'content_out'}=$form{'content_in'};
+$form{'old_content'}=$form{'content_in'};
+$form{'czas'} = 0;
+$res = $ua -> post( 'https://bramka.play.pl/composer/public/editableSmsCompose.do', \%form );
+# SMS confirm
+$res = $ua -> post( 'https://bramka.play.pl/composer/public/editableSmsCompose.do', { SMS_SEND_CONFIRMED => 'Wyślij' } );
+
+print $res->content;
 1;
